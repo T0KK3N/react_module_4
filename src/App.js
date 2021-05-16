22a5a2dde328c6875b700dc6145d6032f1cb54d9
T0KK3N <daniel.mora.ortiz@udla.edu.ec>
2021-05-15 22:09:07 -0500

    Module 2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import TareasLista from './TareasLista'
 
 function App() {
   return (
+    <>
     <TareasLista />
+    <input type="text" />
+    <button>Añadir Tarea</button>
+    <button>Limpiar Tareas Completadas</button>
+    <div>0 tareas por terminar</div>
+    </>
   )
 }
 
