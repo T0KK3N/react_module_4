053845d224e5121a775c431bf8efe54d30619db8
T0KK3N <daniel.mora.ortiz@udla.edu.ec>
2021-05-15 21:52:14 -0500

    Module 1

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import TareasLista from './TareasLista'
+
+function App() {
+  return (
+    <TareasLista />
+  )
+}
+
+export default App;
---
+++ src/TareasLista.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TareasLista() {
+    return (
+        <div>
+            Hello World
+        </div>
+    )
+}
